9d38f7097a6d6683239e178e923c41dddbeb7d1f
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-11 21:07:46 +0200

    add begin file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+import { Container, Heading, Section } from 'components';
+
+export class App extends Component {  
+
+  state = {
+  };  
+
+  render() {
+    return (
+      <Container>
+        <Section>
+          <Heading marginBottom="50px" textAlign="center"></Heading>
+        </Section>
+      </Container>
+    );
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from 'components/App/App';
+export * from 'components/Container/Container.styled.js';
+export * from 'components/Section/Section';
+export * from 'components/Heading/Heading.styled.js';
