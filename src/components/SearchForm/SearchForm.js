18e181f2648631f3fcaeffce2d7bbe9697389b0b
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-13 20:14:47 +0200

    changes SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,24 @@
+// import PropTypes from 'prop-types';
+import {
+  FormBlock,
+  SearchFormButton,
+  SearchFormButtonLabel,
+  SearchFormInput,
+} from './SearchForm.styled';
 
+export const SearchForm = () => {
+  return (
+    <FormBlock>
+      <SearchFormButton type="submit">
+        <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+      </SearchFormButton>
 
-{
-  /* <FormBlock>
-  <SearchFormButton type="submit" >
-    <SearchFormButtonLabel >Search</SearchFormButtonLabel>
-  </SearchFormButton>
-
-  <SearchFormInput
-    type="text"
-    autocomplete="off"
-    autofocus
-    placeholder="Search images and photos"
-  />
-</FormBlock>; */
-}
+      <SearchFormInput
+        type="text"
+        autocomplete="off"
+        autofocus
+        placeholder="Search images and photos"
+      />
+    </FormBlock>
+  );
+};
