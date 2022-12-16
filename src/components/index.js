472bac637fd695c3c38c52782353f7e3a66bf919
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-16 17:44:53 +0200

    update export

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export * from 'components/Container/Container.styled.js';
 export * from 'components/Section/Section.styled';
 export * from 'components/Heading/Heading.styled.js';
 export * from 'components/Searchbar/Searchbar';
+export * from 'components/ImageGallery/ImageGallery';
+export * from 'components/ImageGalleryItem/ImageGalleryItem';
