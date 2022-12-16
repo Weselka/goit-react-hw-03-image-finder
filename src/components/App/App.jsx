72f1c7d62ee3addb8c12c29292904f05a5ee7488
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-16 17:44:30 +0200

    update Searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,88 @@
 import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import axios from 'axios';
 
 import {
   Container,
   // Section,
   Searchbar,
+  ImageGallery,
 } from 'components';
 
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 export class App extends Component {
-  state = {};
+  state = {
+    images: null,
+    imagesName: '',
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch(
+      'https://pixabay.com/api/?key=30883328-4550d73a5a5d91ad50d778095&q=${car}&page=12'
+    )
+      .then(res => res.json())
+      .then(images => this.setState({ images }))
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  handleSearchFormSubmit = imagesName => {
+    // console.log(imagesName);
+    this.setState({ imagesName });
+  };
 
   render() {
     return (
       <>
-        <Searchbar />
+        <Searchbar onSubmit={this.handleSearchFormSubmit} />
         <Container>
-          {/* <Section>            
-          </Section> */}
+          <ImageGallery imagesName={this.state.imagesName}/>
+            {this.state.images && (
+            <div>
+              {this.state.loading && <h1>Download</h1>}
+              {/* {this.state.images} */}
+              images fetch
+            </div>
+          )}
+          {/* <Section></Section> */}
         </Container>
+        <ToastContainer autoClose={3000} />
       </>
     );
   }
 }
+
+// addContact = data => {
+//   const presence = this.state.contacts.some(
+//     contact => contact.name === data.name
+//   );
+//   if (presence) {
+//     Notify.warning(`${data.name} is already in contacts.`);
+//     return;
+//   }
+
+//   this.setState(({ contacts }) => ({
+//     contacts: [{ ...data, id: nanoid() }, ...contacts],
+//   }));
+// };
+
+// deleteContact = contactId => {
+//   this.setState(prevState => ({
+//     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//   }));
+// };
+
+// changesFilter = e => {
+//   this.setState({ filter: e.currentTarget.value });
+// };
+
+// getVisibleContacts = () => {
+//   const { contacts, filter } = this.state;
+//   const normalized = filter.toLowerCase();
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalized)
+//   );
+// };
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,6 @@
-// import PropTypes from 'prop-types';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import {
   SearchbarHeader,
   FormBlock,
@@ -7,20 +9,52 @@ import {
   SearchFormInput,
 } from './Searchbar.styled';
 
-export const Searchbar = () => {
-  return (
-    <SearchbarHeader>
-      <FormBlock>
-        <SearchFormButton type="submit">
-          <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-        </SearchFormButton>
-        <SearchFormInput
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </FormBlock>
-    </SearchbarHeader>
-  );
-};
+export class Searchbar extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+  };
+
+  state = {
+    imagesName: '',
+  };
+
+  handleChange = e => {
+    this.setState({ imagesName: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.imagesName.trim() === '') {
+      return toast.error('Add name');
+      
+    }
+    this.props.onSubmit(this.state.imagesName);
+    this.setState({ imagesName: '' });
+  };
+
+  render() {
+    // const { name } = this.state;
+    return (
+      <SearchbarHeader>
+        <FormBlock onSubmit={this.handleSubmit}>
+          <SearchFormButton type="submit">
+            <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+          </SearchFormButton>
+          <SearchFormInput
+            type="text"
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            // value={name}
+            onChange={this.handleChange}
+            name="imagesName"
+            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            // required
+          />
+        </FormBlock>
+      </SearchbarHeader>
+    );
+  }
+}
