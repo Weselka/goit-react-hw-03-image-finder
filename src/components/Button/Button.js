0757f3433b4fbdd92cf4ce4cceb846661a8c01dd
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-18 22:18:02 +0200

    add load more

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+// import { Button } from './Button.styled';
+
+// export const LoadMore = () => {
+//   //   handleSubmit = e => {
+//   //     e.preventDefault();
+//   //     this.setState({});
+//   //   };
+//   const btnLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   return <Button onClick={this.btnLoadMore}>Load More</Button>;
+// };
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 export * from 'components/App/App';
 export * from 'components/Container/Container.styled.js';
-export * from 'components/Section/Section.styled';
+// export * from 'components/Section/Section.styled';
 export * from 'components/Heading/Heading.styled.js';
 export * from 'components/Searchbar/Searchbar';
 export * from 'components/ImageGallery/ImageGallery';
 export * from 'components/ImageGalleryItem/ImageGalleryItem';
+export * from 'components/Button/Button';
