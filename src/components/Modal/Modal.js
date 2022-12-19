6fae2b7e82f640717616cb8ec8e1be9915324867
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-19 04:14:27 +0200

    add Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,29 @@
-{/* <Overlay class="overlay">
-  <Modal class="modal">
-    <img src="" alt="" />
-  </Modal>
-</Overlay>; */}
+import { Component } from 'react';
+import { Overlay, ModalBox } from './Modal.styled';
+
+export class Modal extends Component {
+  componentDidMount() {
+    console.log('Modal componentDidMount');
+  }
+  componentWillUnmount() {
+    console.log('Modal componentWillUnmount');
+  }
+  render() {
+    return (
+      <Overlay>
+        <ModalBox>{this.props.children}</ModalBox>
+      </Overlay>
+    );
+  }
+}
+
+// export const Modal = ({ images }) => {
+//   console.log(images);
+//   return (
+//     <Overlay >
+//       <ModalBox >
+//         <img src="" alt="" />
+//       </ModalBox>
+//     </Overlay>
+//   );
+// };
--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export * from 'components/Searchbar/Searchbar';
 export * from 'components/ImageGallery/ImageGallery';
 export * from 'components/ImageGalleryItem/ImageGalleryItem';
 export * from 'components/Button/Button';
+export * from 'components/Modal/Modal';
