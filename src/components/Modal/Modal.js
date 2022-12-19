93a5923f49f8f6aacb2010f20d697344194f5ea1
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-19 22:01:32 +0200

    create Portal for Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,34 @@
 import { Component } from 'react';
+import { createPortal } from 'react-dom';
 import { Overlay, ModalBox } from './Modal.styled';
+const modalRoot = document.querySelector('#modal-root');
 
 export class Modal extends Component {
   componentDidMount() {
-    console.log('Modal componentDidMount');
+    window.addEventListener('keydown', this.handleKeyDown);
   }
   componentWillUnmount() {
-    console.log('Modal componentWillUnmount');
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  // handleOverlayClick = e => {
+  //   if (e.currentTarget !== e.target) {
+  //     this.props.onClose();
+  //   }
+  // };
+
   render() {
-    return (
-      <Overlay>
+    return createPortal(
+      <Overlay onClick={this.handleOverlayClick}>
         <ModalBox>{this.props.children}</ModalBox>
-      </Overlay>
+      </Overlay>,
+      modalRoot
     );
   }
 }
